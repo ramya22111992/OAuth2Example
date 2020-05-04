112272fe8603ee4ca55809ab1799eb50b37135d9
ramya <ramya.bala221190@gmail.com>
2020-05-04 15:00:11 +0530

    Third

--- config.js
+++
@@ -1,8 +0,0 @@
-const dotenv=require('dotenv').config({path: './.env'});
-
-module.exports={
-APP_PORT:process.env.APP_PORT,
-CLIENT_ID:process.env.CLIENT_ID,
-CLIENT_SECRET:process.env.CLIENT_SECRET,
-REDIRECT_URI:process.env.REDIRECT_URI
-}
